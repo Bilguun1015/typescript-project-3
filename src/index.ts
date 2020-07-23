df5e7fd405f41a032a08efd8bbaf5ffbc7549bf2
Bilguun <bbg.1015@gmail.com>
2020-07-23 12:42:21 -0500

    fixed the issue that map was not displayin by adding css file, created CustomMap class to make the google.maps to private

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-const user = new User();
-const company = new Company();
+// const user = new User();
+// const company = new Company();
+new CustomMap('map');
