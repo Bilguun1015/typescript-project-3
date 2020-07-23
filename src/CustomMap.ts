41cfa5ad5eec5a27366b46a9ba079eb127a4a13a
Bilguun <bbg.1015@gmail.com>
2020-07-23 14:11:48 -0500

    added 2 methods in CustomMap class to display marker on the page

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
   //cannot access googleMap property outside of this class
   private googleMap: google.maps.Map;
@@ -11,4 +14,16 @@ export class CustomMap {
       },
     });
   }
+  // bad implementation
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {}
 }
